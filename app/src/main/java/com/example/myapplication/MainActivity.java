e2627adb8fe7c9d74ba8a5510757d72bc04cb151
offlinenow <offlinenow@users.noreply.github.com>
2017-09-15 15:17:00 +0800

    Storage

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,9 +3,19 @@ package com.example.myapplication;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
-    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    public final static String EXTRA_MESSAGE = "com.example.myapplication.MESSAGE";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,10 +24,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void login(View view) {
+        try {
+            FileOutputStream fileOutputStream = openFileOutput("text.txt", MODE_PRIVATE);
+            OutputStream out = new BufferedOutputStream(fileOutputStream);
+            String name = ((EditText) findViewById(R.id.EditText1)).getText().toString();
+            String id = ((EditText) findViewById(R.id.EditText2)).getText().toString();
+            String string = "姓名：" + name + " " + "学号：" + id;
 
+            out.write(string.getBytes());
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void PopUpWindow(View view) {
+        int BUFFER_SIZE = 8192;
+        try {
+            FileInputStream fileInputStream = openFileInput("text.txt");
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream, "uft8"), BUFFER_SIZE);
+            Toast.makeText(MainActivity.this, bufferedReader.readLine(), Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
